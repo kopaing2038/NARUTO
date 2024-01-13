f3ea92491c400a768b1e36c29398af7aaf631abb
NARUTO <150582863+Naruto46i@users.noreply.github.com>
2024-01-13 22:25:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -98,6 +98,7 @@ IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 #Openai
 AI = is_enabled((environ.get("AI","True")), True)
 OPENAI_API = environ.get("OPENAI_API","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")
+GOOGLE_API_KEY = os.environ.get('API_KEY', '') # give your Google api key
 AI_LOGS = int(environ.get("AI_LOGS"," ")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
 
