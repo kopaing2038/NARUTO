54ca3e713670fed1b9809ea8e4d9b747d6a0bc63
JAMBOTG <156563417+JAMBOTG@users.noreply.github.com>
2024-01-14 11:03:06 +0530

    Update ask.py

--- plugins/X SUPPORT/ask.py
+++ plugins/X SUPPORT/ask.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 import requests
 from info import LOG_CHANNEL, GOOGLE_API_KEY
-import google.generativeai as genai 
+from google import generativeai as genai 
 
 genai.configure(api_key=GOOGLE_API_KEY) # add your api key in info.py
 
