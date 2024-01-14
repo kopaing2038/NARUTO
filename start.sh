4822596e33f8209425fda54345539706d3d2ef61
NARUTO <150582863+Naruto46i@users.noreply.github.com>
2024-01-14 18:26:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /NARUTO
 fi
-cd /Elsa
+cd /naruto
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜"
 python3 bot.py
